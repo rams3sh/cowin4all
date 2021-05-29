e2f8a7ace1e51c4a59104fd521f487dfdedb3b4f
rams3sh <ramachandransesh@gmail.com>
2021-05-29 22:18:07 +0530

    added char_mapping data

--- setup.py
+++ setup.py
@@ -44,5 +44,9 @@ setuptools.setup(
     packages=["cowin4all", "cowin4all.cowin4all_sdk", "cowin4all.otp_plugins"],
     install_requires=required,
     include_package_data=True,
-    entry_points={'console_scripts': ['cowin4all_app=app:main']},
-    python_requires='>=3.8')
+    package_data={
+        "cowin4all_sdk": ["captcha_char_mapping.json"],
+    },
+    entry_points={'console_scripts': ['cowin4all=cowin4all.app:main']},
+    python_requires='>=3.8'
+    )

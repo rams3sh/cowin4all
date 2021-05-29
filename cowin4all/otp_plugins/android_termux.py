6955844b5e1162f57a9280f0af6530db86261dd0
rams3sh <ramachandransesh@gmail.com>
2021-05-29 23:43:19 +0530

    fixed error in android otp

--- cowin4all/otp_plugins/android_termux.py
+++ cowin4all/otp_plugins/android_termux.py
@@ -56,8 +56,9 @@ def listen_on_new_messages():
             raise Exception("termux-api is not installed !! Error message : {}".format(stderr))
 
         messages = json.loads(stdout)
-
+        print(messages)
         for message in messages:
+            print(message)
             match = re.findall("(?<=CoWIN is )[0-9]{6}", message)
             if match:
                 received_time = datetime.strptime(message["received"], "%Y-%m-%d %H:%M:%S")

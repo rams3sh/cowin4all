5c1425db767942744d63f2872989d1686dabd305
rams3sh <ramachandransesh@gmail.com>
2021-05-30 09:37:46 +0530

    added some exception handling

--- cowin4all/otp_plugins/android_termux.py
+++ cowin4all/otp_plugins/android_termux.py
@@ -40,40 +40,43 @@ def listen_on_new_messages(otp_forwarder_mode=False, url=None):
     otp_wait_time = 150
     last_message_received_time = datetime.now() # Useful only for otp forward mode
 
-    while True:
+    try:
+        while True:
+            if not otp_forwarder_mode:
+                if event_waiter.is_set():
+                    break
 
-        if not otp_forwarder_mode:
-            if event_waiter.is_set():
-                break
+            tmux_sms_list = subprocess.Popen(
+                'termux-sms-list -l 10 -t inbox',
+                stdin=subprocess.DEVNULL,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE, shell=True)
 
-        tmux_sms_list = subprocess.Popen(
-            'termux-sms-list -l 10 -t inbox',
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE, shell=True)
+            output = tmux_sms_list.communicate()
+            stdout = output[0].decode('utf-8')
+            stderr = output[1].decode('utf-8').lower()
+            # Normally the message with "found" or "no" pertains to "command not found / No such commmand etc..".
+            if "found" in stderr or "no" in stderr:
+                otp = None
+                event_waiter.set()
+                raise Exception("termux-api is not installed !! Error message : {}".format(stderr))
 
-        output = tmux_sms_list.communicate()
-        stdout = output[0].decode('utf-8')
-        stderr = output[1].decode('utf-8').lower()
-        # Normally the message with "found" or "no" pertains to "command not found / No such commmand etc..".
-        if "found" in stderr or "no" in stderr:
-            otp = None
-            event_waiter.set()
-            raise Exception("termux-api is not installed !! Error message : {}".format(stderr))
+            messages = json.loads(stdout)
+            for message in messages:
+                match = re.findall("(?<=CoWIN is )[0-9]{6}", message["body"])
+                if match:
+                    received_time = datetime.strptime(message["received"], "%Y-%m-%d %H:%M:%S")
+                    if not otp_forwarder_mode:
+                        if (received_time - time_of_request).seconds <= otp_wait_time:
+                            otp = match[0]
+                            event_waiter.set()
+                            break
+                    else:
+                        if received_time > last_message_received_time:
+                            logger.info("Received OTP Message: {message}. Sending to the webhook service !!"
+                                        "".format(message=message))
+                            requests.put(url=url, json=message)
 
-        messages = json.loads(stdout)
-        for message in messages:
-            match = re.findall("(?<=CoWIN is )[0-9]{6}", message["body"])
-            if match:
-                received_time = datetime.strptime(message["received"], "%Y-%m-%d %H:%M:%S")
-                if not otp_forwarder_mode:
-                    if (received_time - time_of_request).seconds <= otp_wait_time:
-                        otp = match[0]
-                        event_waiter.set()
-                        break
-                else:
-                    if received_time > last_message_received_time:
-                        logger.info("Received OT Message: {message}".format(message=message))
-                        requests.put(url=url, json=message)
-
-        time.sleep(sleep_time)
+            time.sleep(sleep_time)
+    except Exception:
+        logger.error("External Service not accepting connections !! Kindly check the webhook service and restart !!")
